b18560322dad303157a86d431c0e093203bdc9c0
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2023-05-08 22:37:53 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+let n1 = +prompt('Tabuada de qual número?');
+
+let calculo = 
+document.write(n1 + "x","1"," = ", n1 * 1 ,"<br>"); 
+document.write(n1 + "x","2"," = ", n1 * 2 ,"<br>"); 
+document.write(n1 + "x","3"," = ", n1 * 3 ,"<br>"); 
+document.write(n1 + "x","4"," = ", n1 * 4 ,"<br>"); 
+document.write(n1 + "x","5"," = ", n1 * 5 ,"<br>"); 
+document.write(n1 + "x","6"," = ", n1 * 6 ,"<br>"); 
+document.write(n1 + "x","7"," = ", n1 * 7 ,"<br>"); 
+document.write(n1 + "x","8"," = ", n1 * 8 ,"<br>"); 
+document.write(n1 + "x","9"," = ", n1 * 9 ,"<br>"); 
+document.write(n1 + "x","10"," = ", n1 * 10 ,"<br>"); 
